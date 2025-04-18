f668371b418dac54408d08fb59484e8f406cac07
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2025-04-18 20:12:37 +0300

    Added indexes

--- src/main/java/com/xm/recommendationservice/infrastructure/repository/entities/CryptoPriceEntity.java
+++ src/main/java/com/xm/recommendationservice/infrastructure/repository/entities/CryptoPriceEntity.java
@@ -7,7 +7,11 @@ import java.time.Instant;
 
 @Entity
 @Table(name = "crypto_prices", uniqueConstraints = {
-        @UniqueConstraint(columnNames = { "symbol", "timestamp" })
+        @UniqueConstraint(name = "uc_crypto_symbol_timestamp", columnNames = { "symbol", "timestamp" })
+}, indexes = {
+        @Index(name = "idx_symbol", columnList = "symbol"),
+        @Index(name = "idx_timestamp", columnList = "timestamp"),
+        @Index(name = "idx_symbol_timestamp", columnList = "symbol, timestamp")
 })
 @Getter
 @Setter
